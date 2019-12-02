31467c72de3a6570c3fa7a9a09a197966a82586f
setvect <setvect@naver.com>
2019-12-02 23:02:56 +0900

    git test

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -4,7 +4,7 @@
     <main>
       <div class="left-side">
         <span class="title">
-          Welcome to your new project!
+          안녕 복슬이
         </span>
         <system-information></system-information>
       </div>
