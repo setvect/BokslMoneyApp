b5a94ee2b8608230112a91b9ff4bc2e21d212eb7
unknown <setvect@naver.com>
2020-01-03 13:35:15 +0900

    NEW-20200103: ESLINT 최신 버전으로 업데이트

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,24 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
     sourceType: 'module',
   },
   env: {
     browser: true,
+    es6: true,
     node: true,
   },
-  extends: 'standard',
+  extends: ['eslint:recommended', 'plugin:vue/essential'],
   globals: {
     __static: true,
     Swal: true,
   },
-  plugins: ['html'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['html', 'vue'],
   rules: {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
-    'comma-dangle': [
-      'off',
-      {
-        arrays: 'off',
-        objects: 'off',
-        imports: 'off',
-        exports: 'off',
-        functions: 'off',
-      },
-    ],
-    'space-before-function-paren': ['off'],
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+
   },
 }
