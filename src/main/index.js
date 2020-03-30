260fc957568f39b297cb6920632aed31bd5e0a5b
setvect <setvect@naver.com>
2020-03-30 21:40:37 +0900

    디버깅 창 표시

--- src/main/index.js
+++ src/main/index.js
@@ -1,33 +1,37 @@
-import { app, BrowserWindow } from 'electron'
-import path from 'path'
-import { format as formatUrl } from 'url'
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import { format as formatUrl } from "url";
 
-const isDevelopment = process.env.NODE_ENV !== 'production'
-console.log('isDevelopment :', isDevelopment);
+const isDevelopment = process.env.NODE_ENV !== "production";
+console.log("isDevelopment :", isDevelopment);
 
-app.on('ready', () => {
+app.on("ready", () => {
   let window = new BrowserWindow({
     width: 1024,
     webPreferences: {
       nodeIntegration: true
     }
-  })
+    // 개발자 도구를 엽니다.
+  });
   if (isDevelopment) {
-    window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`)
+    window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`);
   } else {
-    window.loadURL(formatUrl({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file',
-      slashes: true
-    }))
+    window.loadURL(
+      formatUrl({
+        pathname: path.join(__dirname, "index.html"),
+        protocol: "file",
+        slashes: true
+      })
+    );
   }
   window.on("closed", () => {
     window = null;
-  })
-})
+  });
+  window.webContents.openDevTools();
+});
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
   }
-})
+});
