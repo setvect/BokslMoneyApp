877c4bb51f4ff77ab522c3fefa6efad6dd96bf10
setvect <setvect@naver.com>
2020-05-03 17:16:12 +0900

    코드관리 - 작업중

--- src/main/index.js
+++ src/main/index.js
@@ -48,7 +48,40 @@ categoryVo.sync().then(() => console.log("category .."));
 oftenUsedVo.sync().then(() => console.log("oftenUsed .."));
 memoVo.sync().then(() => console.log("memo .."));
 transactionVo.sync().then(() => console.log("transaction .."));
-codeMainVo.sync().then(() => console.log("codeMain .."));
+codeMainVo
+  .sync()
+  .then(() => {
+    return codeMainVo.findAll();
+  })
+  .then((codeMain) => {
+    if (codeMain.length !== 0) {
+      return null;
+    }
+    // 기본 사용자 등록
+    return codeMainVo.bulkCreate([
+      {
+        codeMainId: "KIND_CODE",
+        name: "자산유형",
+        deleteF: false,
+      },
+      {
+        codeMainId: "ATTR_SPENDING",
+        name: "지출속성",
+        deleteF: false,
+      },
+      {
+        codeMainId: "ATTR_TRANSFER",
+        name: "이체속성",
+        deleteF: false,
+      },
+      {
+        codeMainId: "ATTR_INCOME",
+        name: "수입속성",
+        deleteF: false,
+      }
+    ]);
+  })
+  .then(() => console.log("codeMain .."));
 codeItemVo.sync().then(() => console.log("codeItem .."));
 
 loginEvent.init();
