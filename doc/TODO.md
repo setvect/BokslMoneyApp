d93c9d9d86332a788b408c8c43845bd11bfda46b
setvect <setvect@naver.com>
2020-06-28 23:35:56 +0900

    카테고리 조회

--- doc/TODO.md
+++ doc/TODO.md
@@ -3,6 +3,7 @@
 ### 메모
 
 ### 진행예정
+- NEW-20200628: category sequelize의 외래키 설정 이용해 parentCategory 가져오기
 - PBM-20200522: 두번 연속 팝업창 오픈하면 팝업창 요소가 클릭안되는 문제 - 발생하는 조건을 모르겠음.
 - NEW-20200522: 컴포넌트 옵션 순서 권고사항으로 변경: https://kr.vuejs.org/v2/style-guide/index.html#%EC%BB%B4%ED%8F%AC%EB%84%8C%ED%8A%B8-%EC%9D%B8%EC%8A%A4%ED%84%B4%EC%8A%A4-%EC%98%B5%EC%85%98-%EC%88%9C%EC%84%9C-%EC%B6%94%EC%B2%9C%ED%95%A8
 - NEW-20200512: 페이지 전체 최소 넓이 지정
@@ -18,6 +19,7 @@
 - NEW-20200101: 통계
 
 ### 완료
+
 - REF-20200522: 글로벌 이벤트 버스 사용하지 않기
 - NEW-20200101(20200509): 분류 관리
 - NEW-20200412(20200509): vue파일에 포메팅 했을 때 세미콜론; 추가하도록함
@@ -25,12 +27,12 @@
 - NEW-20200505(20200507): 계좌 관리 - 엑셀 다운로드
 - NEW-20200101: 계좌 관리
 - NEW-20200101(20200504): 코드 관리
-- PBM-20200405(20200429): Deprecation warning: value provided is not in a recognized RFC2822 or ISO format. moment construction falls back to js Date(),  ... warning 해결
-- NEW-20200419: eslint *.vue 파일에도 작동할 수 있도록
+- PBM-20200405(20200429): Deprecation warning: value provided is not in a recognized RFC2822 or ISO format. moment construction falls back to js Date(), ... warning 해결
+- NEW-20200419: eslint \*.vue 파일에도 작동할 수 있도록
 - NEW-20200405(20200419): eslint 셋팅, 사용.
 - NEW-20200101(20200411): 기본 화면 구성
 - NEW-20200407: 새로고침 단축키 적용
-- PBM-20200404: [Vue warn]: $attrs is readonly 오류 해결. bootstrap-vue 문제로 판단됨.
+- PBM-20200404: [Vue warn]: \$attrs is readonly 오류 해결. bootstrap-vue 문제로 판단됨.
 - NEW-20200329(20200330): 간단한 DB 연동. sqlite3, sequelize
 - NEW-20200329(20200330): 필요없는 메뉴 제거
 - NEW-20200329: electron-webpack 사용으로 인해 vscode를 사용한 디버깅이 안되는 문제 해결
--- src/main/module/category/categorytService.js
+++ src/main/module/category/categorytService.js
@@ -5,6 +5,9 @@ import category from "../../model/category-vo";
 import transactionService from "../transaction/transactionService.js";
 import moment from "moment";
 import _ from "lodash";
+import {
+  Op
+} from "sequelize";
 
 export default {
   init() {
@@ -24,29 +27,61 @@ export default {
 
     // 메인 분류 목록
     ipcMain.handle("category/list", async(event, param) => {
-      const defaultCondition = {
-        deleteF: false,
-      };
-      const where = {
-        ...defaultCondition,
-        ...param,
-      };
-      const result = await category.findAll({
-        where,
-        order: ["orderNo"],
-        raw: true,
-      });
-      return result;
+      return this.list(param);
     });
 
+    // 자주 사용하는 카테고리 조회
     ipcMain.handle("/category/listRecommend", async(event, param) => {
+      let categorySeqMap = await this.findByCategoryName({
+        kind: param.kind,
+        name: param.note,
+      });
+
       param.from = (moment()).add(-100, "days").toDate();
       param.to = new Date();
       param.returnCount = 1000;
 
       let list = await transactionService.list(param);
-      let countBy = _.countBy(list, "categorySeq");
-      console.log("countBy :>> ", countBy);
+      let categoryMap = _.countBy(list, "categorySeq");
+      console.log("categorySeqMap :>> ", categorySeqMap);
+      console.log("categoryMap :>> ", categoryMap);
+
+      let mergeCategorySeq = {};
+      _(categorySeqMap).forEach((value, key) => {
+        let v = mergeCategorySeq[key];
+        if (v == null) {
+          mergeCategorySeq[key] = value;
+        } else {
+          mergeCategorySeq[key] += value;
+        }
+      });
+
+      _(categoryMap).forEach((value, key) => {
+        let v = mergeCategorySeq[key];
+        if (v == null) {
+          mergeCategorySeq[key] = value;
+        } else {
+          mergeCategorySeq[key] += value;
+        }
+      });
+
+      let sortCategories = Object.keys(mergeCategorySeq).sort((a, b) => {
+        return mergeCategorySeq[b] - mergeCategorySeq[a];
+      });
+
+      // 자주 사용하는 카테고리
+      console.log("sortCategories :>> ", sortCategories);
+      let findCategory = await category.findAll({
+        where: {
+          "categorySeq": {
+            [Op.in]: sortCategories,
+          },
+        },
+        raw: true,
+      });
+      console.log("findCategory :>> ", findCategory);
+
+      // TODO 카테고리 정렬 해야됨. sortCategories 순서 기준
 
       return list;
     });
@@ -76,13 +111,40 @@ export default {
       await upItem.save();
     });
 
+    // ================ 삭제 ================
     ipcMain.handle("category/deleteItem", async(event, categorySeq) => {
       const saveItem = await category.findByPk(categorySeq);
       saveItem.deleteF = true;
       saveItem.save();
     });
+  },
 
+  async findByCategoryName(param) {
+    let categoryList = await this.list({
+      kind: param.kind,
+      name: {
+        [Op.like]: `%${param.name}%`,
+      },
+    });
+    let categorySeqMap = _.chain(categoryList).keyBy("categorySeq").mapValues(() => 1000).value();
+    return categorySeqMap;
+  },
+  async list(param) {
+    const defaultCondition = {
+      deleteF: false,
+    };
+    const where = {
+      ...defaultCondition,
+      ...param,
+    };
 
-    // ================ 삭제 ================
+    console.log("where@@@@@@@@@@@@@@ :>> ", where);
+    const result = await category.findAll({
+      where,
+      order: ["orderNo"],
+      raw: true,
+    });
+    return result;
+
   },
 };
