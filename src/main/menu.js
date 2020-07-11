8b3048030d328926724f08fd0da1749254ca4fc4
setvect <setvect@naver.com>
2020-07-11 17:41:20 +0900

    매뉴 뎁스 2->1로. 단축키 F1, F2, ... 이런식으로

--- src/main/menu.js
+++ src/main/menu.js
@@ -1,90 +1,85 @@
-import { Menu } from "electron";
+import {
+  Menu
+} from "electron";
 
 export default {
   init() {
-    const template = [
-      {
-        label: "기능",
-        submenu: [
-          {
-            label: "가계부 쓰기(달력)",
-            accelerator: "Ctrl+F1",
-            click(item, focusedWindow) {
-              focusedWindow.webContents.send("movePage", "calendar");
-            },
-          },
-          {
-            label: "가계부 쓰기(표)",
-            accelerator: "Ctrl+F2",
-            click(item, focusedWindow) {
-              focusedWindow.webContents.send("movePage", "grid");
-            },
-          },
-          {
-            label: "결산",
-            accelerator: "Ctrl+F3",
-            click(item, focusedWindow) {
-              focusedWindow.webContents.send("movePage", "settlement");
-            },
-          },
-          {
-            label: "통계",
-            accelerator: "Ctrl+F4",
-            click(item, focusedWindow) {
-              focusedWindow.webContents.send("movePage", "stat");
-            },
-          },
-          {
-            label: "분류 관리",
-            accelerator: "Ctrl+F5",
-            click(item, focusedWindow) {
-              focusedWindow.webContents.send("movePage", "category");
-            },
-          },
-          {
-            label: "계좌 관리",
-            accelerator: "Ctrl+F6",
-            click(item, focusedWindow) {
-              focusedWindow.webContents.send("movePage", "account");
-            },
-          },
-          {
-            label: "코드 관리",
-            accelerator: "Ctrl+F7",
-            click(item, focusedWindow) {
-              focusedWindow.webContents.send("movePage", "code", {
-                mainCode: "KIND_CODE",
-              });
-            },
-          }
-        ],
+    const template = [{
+      label: "가계부 쓰기(달력)",
+      accelerator: "Ctrl+F1",
+      click(item, focusedWindow) {
+        focusedWindow.webContents.send("movePage", "calendar");
+      },
+    },
+    {
+      label: "가계부 쓰기(표)",
+      accelerator: "Ctrl+F2",
+      click(item, focusedWindow) {
+        focusedWindow.webContents.send("movePage", "grid");
+      },
+    },
+    {
+      label: "결산",
+      accelerator: "Ctrl+F3",
+      click(item, focusedWindow) {
+        focusedWindow.webContents.send("movePage", "settlement");
+      },
+    },
+    {
+      label: "통계",
+      accelerator: "Ctrl+F4",
+      click(item, focusedWindow) {
+        focusedWindow.webContents.send("movePage", "stat");
+      },
+    },
+    {
+      label: "분류 관리",
+      accelerator: "Ctrl+F5",
+      click(item, focusedWindow) {
+        focusedWindow.webContents.send("movePage", "category");
+      },
+    },
+    {
+      label: "계좌 관리",
+      accelerator: "Ctrl+F6",
+      click(item, focusedWindow) {
+        focusedWindow.webContents.send("movePage", "account");
+      },
+    },
+    {
+      label: "코드 관리",
+      accelerator: "Ctrl+F7",
+      click(item, focusedWindow) {
+        focusedWindow.webContents.send("movePage", "code", {
+          mainCode: "KIND_CODE",
+        });
+      },
+    },
+    {
+      role: "Help",
+      submenu: [{
+        label: "Reload",
+        accelerator: "Ctrl+Shift+R",
+        click(item, focusedWindow) {
+          focusedWindow.reload();
+        },
       },
       {
-        role: "Help",
-        submenu: [
-          {
-            label: "Reload",
-            accelerator: "Ctrl+Shift+R",
-            click(item, focusedWindow) {
-              focusedWindow.reload();
-            },
-          },
-          {
-            label: "Toggle DevTools",
-            click(item, focusedWindow) {
-              focusedWindow.toggleDevTools();
-            },
-          },
-          {
-            label: "Learn More",
-            click() {
-              require("electron").shell.openExternal(
-                "https://github.com/setvect/BokslMoneyApp"
-              );
-            },
-          }
-        ],
+        label: "Toggle DevTools",
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        },
+      },
+      {
+        label: "Learn More",
+        click() {
+          require("electron").shell.openExternal(
+            "https://github.com/setvect/BokslMoneyApp"
+          );
+        },
       }
+      ],
+    }
     ];
 
     const menu = Menu.buildFromTemplate(template);
